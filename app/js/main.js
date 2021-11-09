14fd537755e4e7c44ab3cb9d6ef4a5badfb78081
V-Svistunova <wika1706@gmail.com>
2021-11-09 22:54:25 +0300

    feat: add autoplaySpeed and pauseOnHover

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,14 @@ $(function () {
 
   $('.review__slider').slick({
     arrows: false,
+    initialSlide: 1,
     autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    //slidesToShow: 1.05,
+    centerMode: true,
+    variableWidth: true,
+    
   });
 })
