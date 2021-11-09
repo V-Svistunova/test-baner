a09bf2f73a316f2c3128fce705332fb73f186bb9
V-Svistunova <wika1706@gmail.com>
2021-11-10 00:39:58 +0300

    feat: add dark theme

--- app/js/main.js
+++ app/js/main.js
@@ -7,9 +7,37 @@ $(function () {
     autoplaySpeed: 5000,
     pauseOnFocus: true,
     pauseOnHover: true,
-    //slidesToShow: 1.05,
     centerMode: true,
     variableWidth: true,
-    
   });
 })
+
+
+
+
+function toggleTheme() {
+  let closeBtn = document.querySelector('.header__btn');
+  let footer = document.querySelector('.footer');
+  let subscribe = document.querySelector('.main');
+  let subscribeBlock1 = document.querySelector('.subscribe__block-1');
+  let subscribeBlock2 = document.querySelector('.subscribe__block-2');
+  let subscribeBlock3 = document.querySelector('.subscribe__block-3');
+  
+  let slideReview1 = document.querySelector('.review__block-1');
+  let slideReview2 = document.querySelector('.review__block-2');
+  let slideReview3 = document.querySelector('.review__block-3');
+
+  closeBtn.addEventListener('click', function() {
+    
+    slideReview1.classList.toggle('review__block--bg-dark');
+    slideReview2.classList.toggle('review__block--bg-dark');
+    slideReview3.classList.toggle('review__block--bg-dark');
+    subscribeBlock1.classList.toggle('subscribe__block--bg-dark');
+    subscribeBlock2.classList.toggle('subscribe__block--bg-dark');
+    subscribeBlock3.classList.toggle('subscribe__block--bg-dark');
+    subscribe.classList.toggle('bg--black');
+    footer.classList.toggle('bg--black');
+  })
+}
+
+toggleTheme();
