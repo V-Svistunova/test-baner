6f418355ba61c702ba6500e9fc0f9d82b203f534
V-Svistunova <wika1706@gmail.com>
2021-11-11 17:06:46 +0300

    feat: add click from 3 link

--- app/js/main.js
+++ app/js/main.js
@@ -12,20 +12,48 @@ $(function () {
   });
 })
 
+
+
+
+
 let subscribeBlock1 = document.querySelector('.subscribe__block-1');
 let subscribeBlock2 = document.querySelector('.subscribe__block-2');
 let subscribeBlock3 = document.querySelector('.subscribe__block-3');
+let closeBtn = document.querySelector('.header__btn');
+let footer = document.querySelector('.footer');
+let subscribe = document.querySelector('.main');
+let slideReview1 = document.querySelector('.review__block-1');
+let slideReview2 = document.querySelector('.review__block-2');
+let slideReview3 = document.querySelector('.review__block-3');
+
+
+
+function toggleActiveBtn() {
+  subscribeBlock1.addEventListener('click' , function() {
+    subscribeBlock1.classList.add('subscribe__block--active');
+    subscribeBlock2.classList.remove('subscribe__block--active');
+    subscribeBlock3.classList.remove('subscribe__block--active');
+  })
+
+  subscribeBlock2.addEventListener('click' , function() {
+    subscribeBlock2.classList.add('subscribe__block--active');
+    subscribeBlock1.classList.remove('subscribe__block--active');
+    subscribeBlock3.classList.remove('subscribe__block--active');
+  })
+
+  subscribeBlock3.addEventListener('click' , function() {
+    subscribeBlock3.classList.add('subscribe__block--active');
+    subscribeBlock2.classList.remove('subscribe__block--active');
+    subscribeBlock1.classList.remove('subscribe__block--active');
+  })
+
+
+}
+
 
 
 function toggleTheme() {
-  let closeBtn = document.querySelector('.header__btn');
-  let footer = document.querySelector('.footer');
-  let subscribe = document.querySelector('.main');
   
-  
-  let slideReview1 = document.querySelector('.review__block-1');
-  let slideReview2 = document.querySelector('.review__block-2');
-  let slideReview3 = document.querySelector('.review__block-3');
 
   closeBtn.addEventListener('click', function() {
     
@@ -41,11 +69,17 @@ function toggleTheme() {
 }
 
 toggleTheme();
+toggleActiveBtn()
 
 
-
-
-
+function go() {
+  var radios=document.getElementById("radios").getElementsByTagName("INPUT"),
+      radio
+  for (var i=0, L=radios.length; i<L; i++) {
+      radio=radios[i]
+      if (radio.checked) {window.open(href=radio.value); break}
+  }
+}
 
 
 
